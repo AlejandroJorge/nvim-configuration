278ded078f17c487c90f174f862cb12580c45162
AlejandroJorge <a20202060@pucp.edu.pe>
2024-02-05 23:50:46 -0500

    Finally fixed formatting and lsp

--- lua/custom/plugins/autosave.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "pocco81/auto-save.nvim",
-  config = {
-    vim.api.nvim_set_keymap("n", "<leader>ta", ":ASToggle<CR>", { desc = "Toggle autosave" })
-  }
-}
---
+++ lua/custom/plugins/format.lua
@@ -0,0 +1,26 @@
+return {
+  "mhartington/formatter.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("formatter").setup({
+      filetype = {
+        javascript = {
+          require("formatter.filetypes.javascript").prettier
+        },
+        javascriptreact = {
+          require("formatter.filetypes.javascriptreact").prettier
+        },
+        typescript = {
+          require("formatter.filetypes.typescript").prettier
+        },
+        typescriptreact = {
+          require("formatter.filetypes.typescriptreact").prettier
+        }
+      }
+    })
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      command = "FormatWriteLock"
+    })
+  end
+}
