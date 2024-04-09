c2349a6be4bc609523d1aa0825c491d95ffa6faf
AlejandroJorge <a20202060@pucp.edu.pe>
2024-04-09 17:49:26 -0500

    Added gitblame

---
+++ lua/custom/plugins/gitblame.lua
@@ -0,0 +1,7 @@
+return {
+  "f-person/git-blame.nvim",
+  config = function()
+    require("gitblame").setup({ enabled = false })
+    vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>GitBlameToggle<CR>', { noremap = true, silent = true })
+  end
+}
